f1e5dffcbd946022c871ba1d361abb9a1be8d9e6
Cristian Louzado <cris.s.l.s@hotmail.com>
2023-01-04 14:47:04 -0300

    Class Position ended.

---
+++ src/application/Pogram.java
@@ -0,0 +1,13 @@
+package application;
+
+import board_layer.Position;
+
+public class Pogram {
+
+	public static void main(String[] args) {
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
