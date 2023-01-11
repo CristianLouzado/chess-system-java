2757a99bf02e4623776418e6e10bbf2b4fc63b2e
Cristian Louzado <cris.s.l.s@hotmail.com>
2023-01-11 16:12:20 -0300

    Initializing chessPosition and the system is not broken anymore:)

--- src/board_layer/Board.java
+++ src/board_layer/Board.java
@@ -5,7 +5,7 @@ public class Board {
 	private int columns;
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
-		if (rows<1|| columns<1) {
+		if (rows<1 || columns<1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column."); 
 		}
 		this.rows = rows;
@@ -22,7 +22,7 @@ public class Board {
 		if (!positionExists(row,column)) {
 			throw new BoardException("Position not on the board.");
 		}
-		return pieces [row][columns];
+		return pieces [row][column];
 	}
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
@@ -38,13 +38,13 @@ public class Board {
 		piece.position=position;
 	}
 	public boolean positionExists(int row, int column) {
-		return row>=0&&row<rows&&column>=0&&column<column;
+		return row >= 0 && row <rows && column >= 0 && column < columns;
 	}
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(),position.getColumn());
 	}
 	public boolean thereIsAPiece(Position position) {
-		if (positionExists(position)) {
+		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board.");
 		}
 		return piece(position)!=null;
