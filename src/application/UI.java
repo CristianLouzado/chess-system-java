e9ba152664403c89ee73da31ceb44c177aeb80e5
Cristian Louzado <cris.s.l.s@hotmail.com>
2023-01-18 16:17:03 -0300

    Add turns and current player.

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess_layer.ChessMatch;
 import chess_layer.ChessPiece;
 import chess_layer.ChessPosition;
 import chess_layer.Color;
@@ -44,6 +45,12 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
+	}
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn : "+ chessMatch.getTurn());
+		System.out.println("Waiting player: "+ chessMatch.getCurrentPlayer());
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
